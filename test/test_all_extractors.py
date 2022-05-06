f642ada84f076ec0a3e72ff46c4f2f71ea5a44c8
aditya-sigmoid <98586295+aditya-sigmoid@users.noreply.github.com>
2022-05-06 09:49:29 +0530

    Update test_all_extractors.py

--- test/test_all_extractors.py
+++ test/test_all_extractors.py
@@ -1,16 +1,16 @@
 import unittest
 
-# from test.test_country_code import TestCountryCode
-# from test.test_covid_keyword import TestCovidKeywords
-# from test.test_donation_amount_and_currency import TestDonationDetails
-# from test.test_prevention_keyword import TestPreventionKeywords
-# from test.test_trending_keywords import TestTrendingKeywords
+from test.test_country_code import TestCountryCode
+from test.test_covid_keyword import TestCovidKeywords
+from test.test_donation_amount_and_currency import TestDonationDetails
+from test.test_prevention_keyword import TestPreventionKeywords
+from test.test_trending_keywords import TestTrendingKeywords
 
-from test import TestCountryCode
-from test import TestCovidKeywords
-from test import TestDonationDetails
-from test import TestPreventionKeywords
-from test import TestTrendingKeywords
+# from test import TestCountryCode
+# from test import TestCovidKeywords
+# from test import TestDonationDetails
+# from test import TestPreventionKeywords
+# from test import TestTrendingKeywords
 
 
 class TestAllConsumers(unittest.TestCase):
