45e07415f0bca25487306dea23c1720539af764a
aditya-sigmoid <adityag1@sigmoidanalytics.com>
2022-05-26 17:18:47 +0530

    metadata

--- src/airflow-dags/test_greet.py
+++ src/airflow-dags/test_greet.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
-import re
+
+from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
@@ -29,19 +30,22 @@ mongo_conn = mongodb_connection()
 db_name = "tweet_db"
 db = mongo_conn[db_name]
 coll = db["test_airflow"]
+coll_name = db['metadata table']
+
+
 
 LOGGER = logging.getLogger(__name__)
 
 def duplicate_country(ti):
     li = []
-    mess1 = {'_id': '1226724199133409281',
+    mess1 = {'_id': '142226724199133409281',
              'tweet': '@DarylTractor Oh you beat @WHO me to it! corona donation Not a good $ 2332 sanitiser look unemployment smiling while PM is mask talking about deaths! Attention span of a gnat has #StephAsher',
              'country': 'Australia', 'created_at': '2022-05-18 03:58:29'}
 
-    mess2 = {'_id': '1626776199537409281',
+    mess2 = {'_id': '23223376199537409281',
              'tweet': '@Stanneel Oh you beat me @WHO to it! Not a good look donation smiling $2222 while PM is talking mask corona about deaths! Attention span of a gnat has #StephAsher',
-             'country': 'Australia', 'created_at': '2022-05-19 03:58:29'}
-    mess3 = {'_id': '1626356199537409281',
+             'country': 'China', 'created_at': '2022-05-19 03:58:29'}
+    mess3 = {'_id': '82222356199537409281',
              'tweet': '@aditya Oh you beat me to it! @WHO Not a good look smiling donation $123 while sanitiser corona PM is unemployment talking about deaths! Attention span of a gnat has #StephAsher',
              'country': 'India', 'created_at': '2022-05-18 03:58:29'}
 
@@ -50,9 +54,25 @@ def duplicate_country(ti):
     li.append(mess3)
     li_message = parse_country_codes(li)
     message_list = ti.xcom_push(key='message_list', value=li_message)
-    print('heelo ,',message_list)
+    print('hello ,',message_list)
 
 
+def collection_for_validation(ti):
+
+    messages = ti.xcom_pull(task_ids='get_country_code', key='message_list')
+
+    start_datetime = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    list_ids = []
+    for message in messages:
+        list_ids.append(message['_id'])
+
+    date_time_list_id = {}
+    date_time_list_id['start_datetime'] = start_datetime
+    date_time_list_id['record_ids']=list_ids
+
+    ti.xcom_push(key='datetime_record', value=date_time_list_id)
+    coll_name.insert_one({'start_datetime':start_datetime,'record_ids':list_ids})
+    print('inserted')
 
 def duplicate_covid_keywords(ti):
 
@@ -60,6 +80,10 @@ def duplicate_covid_keywords(ti):
     li_message = parse_covid_keywords(message)
     updated_message_list = ti.xcom_push(key='message_list', value=li_message)
     print(li_message)
+
+
+
+
 
 def duplicate_donation_data(ti):
 
@@ -110,26 +134,140 @@ def insert_mongo(ti):
 
 
 ### now analytics part
-#######################################
+#########################################################################
 
 def total_tweets(ti) :
 
+    # {'IN 05 2022': 2, 'GB 05 2022': 1}
+
     message = ti.xcom_pull(task_ids='keyword_data',key='message_list')
+    date_time_list = ti.xcom_pull(task_ids = 'make_collection', key = 'datetime_record')
+    start_datetime = date_time_list['start_datetime']
     li_message = updated_list_total_tweets(message,db)
+
     print(li_message)
+
+    batch_list = list(coll_name.find().sort('start_datetime',-1))
+    if len(batch_list) == 1:
+        before_batch = batch_list[0]
+    else:
+        before_batch = batch_list[1]
+
+    print('before start time ', before_batch['start_datetime'])
+    print('abhi wala',start_datetime)
+
+    if before_batch['start_datetime'] == start_datetime:
+        print('1st')
+        coll_name.update_one({'start_datetime':start_datetime},{'$set':{'total_tweets_before':'Null','total_tweets_after':li_message}})
+    else:
+        temp_dict = before_batch['total_tweets_after']
+        print('purana')
+        for key,value in temp_dict.items():
+            if key not in li_message.keys():
+                li_message[key] = value
+            else:
+                li_message[key] += value
+
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'total_tweets_before': temp_dict, 'total_tweets_after': li_message}})
 
 
 def tweets_daily_basis(ti) :
+
+    # {'IN 2022-05-18': 2, 'GB 2022-05-19': 1}
 
     message = ti.xcom_pull(task_ids='keyword_data',key='message_list')
     li_message = updated_list_daily_tweets(message,db)
     print('hello',li_message)
 
+    date_time_list = ti.xcom_pull(task_ids='make_collection', key='datetime_record')
+    start_datetime = date_time_list['start_datetime']
+
+    batch_list = list(coll_name.find().sort('start_datetime', -1))
+    if len(batch_list) == 1:
+        before_batch = batch_list[0]
+    else:
+        before_batch = batch_list[1]
+
+    print('before start time ', before_batch['start_datetime'])
+    print('abhi wala', start_datetime)
+
+    if before_batch['start_datetime'] == start_datetime:
+        print('1st')
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'tweet_daily_before': 'Null', 'tweet_daily_after': li_message}})
+
+    else:
+        temp_dict = before_batch['tweet_daily_after']
+        print('purana')
+        for key,value in temp_dict.items():
+            if key not in li_message.keys():
+                li_message[key] = value
+            else:
+                li_message[key] += value
+
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'tweet_daily_before': temp_dict, 'tweet_daily_after': li_message}})
+
+
+################################################################################
 
 def get_top_preventions(ti):
+
+    # {'IN': [{'Sanitiser': 2}, {'Mask': 1}], 'GB': [{'Mask': 1}]}
+
     message = ti.xcom_pull(task_ids='keyword_data', key='message_list')
-    li_message = updated_list_top_10_precautions(message, db)
-    print(li_message)
+    new_dict = updated_list_top_10_precautions(message, db)
+    print(new_dict)
+
+    date_time_list = ti.xcom_pull(task_ids='make_collection', key='datetime_record')
+    start_datetime = date_time_list['start_datetime']
+
+    batch_list = list(coll_name.find().sort('start_datetime', -1))
+    if len(batch_list) == 1:
+        before_batch = batch_list[0]
+    else:
+        before_batch = batch_list[1]
+
+    print('before start time ', before_batch['start_datetime'])
+    print('abhi wala', start_datetime)
+
+    if before_batch['start_datetime'] == start_datetime:
+        print('1st')
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'prevention_before': 'Null', 'prevention_after': new_dict}})
+
+    else:
+
+        # {'IN': [{'Sanitiser': 2}, {'Mask': 1}], 'GB': [{'Mask': 1}]}
+
+        temp_dict = before_batch['prevention_after']
+
+        for key, value in temp_dict.items():
+            if key not in new_dict.keys():
+                new_dict[key] = value
+
+            else:
+                updated_dict = {}
+                old_list = temp_dict[key]
+                for dictionary in old_list:
+                    for key1, val1 in dictionary.items():
+                        updated_dict[key1] = val1
+
+                new_list = new_dict[key]
+                for dictionary in new_list:
+                    for key1, val1 in dictionary.items():
+                        updated_dict[key1] = updated_dict.get(key1, 0) + val1
+
+                new_updated_list = []
+                for key1, val1 in updated_dict.items():
+                    new_updated_list.append({key1: val1})
+
+                new_dict[key] = new_updated_list
+
+
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'prevention_before': temp_dict, 'prevention_after': new_dict}})
 
 
 def get_trend_data(ti):
@@ -140,17 +278,168 @@ def get_trend_data(ti):
     print('covid', trend_list[0])
     print('economy',trend_list[1])
 
+    covid_trend_list = trend_list[0]
+    economy_trend_list = trend_list[1]
+
+
+
+    ###################covid############################################
+
+    date_time_list = ti.xcom_pull(task_ids='make_collection', key='datetime_record')
+    start_datetime = date_time_list['start_datetime']
+
+    batch_list = list(coll_name.find().sort('start_datetime', -1))
+    if len(batch_list) == 1:
+        before_batch = batch_list[0]
+    else:
+        before_batch = batch_list[1]
+
+    print('before start time ', before_batch['start_datetime'])
+    print('abhi wala', start_datetime)
+
+    if before_batch['start_datetime'] == start_datetime:
+        print('1st')
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'covid_trend_before': 'Null', 'covid_trend_after': covid_trend_list}})
+
+    else:
+        temp_dict = before_batch['covid_trend_after']
+        print('purana')
+        for key, value in temp_dict.items():
+            if key not in covid_trend_list.keys():
+                covid_trend_list[key] = value
+            else:
+                covid_trend_list[key] += value
+
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'donation_before': temp_dict, 'donation_after': covid_trend_list}})
+
+    #################end covid ##################################################
+
+    ######################economy ###############################################
+
+    date_time_list = ti.xcom_pull(task_ids='make_collection', key='datetime_record')
+    start_datetime = date_time_list['start_datetime']
+
+    batch_list = list(coll_name.find().sort('start_datetime', -1))
+    if len(batch_list) == 1:
+        before_batch = batch_list[0]
+    else:
+        before_batch = batch_list[1]
+
+    print('before start time ', before_batch['start_datetime'])
+    print('abhi wala', start_datetime)
+
+    if before_batch['start_datetime'] == start_datetime:
+        print('1st')
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'covid_trend_before': 'Null', 'covid_trend_after': covid_trend_list}})
+
+    else:
+        temp_dict = before_batch['covid_trend_after']
+        print('purana')
+        for key, value in temp_dict.items():
+            if key not in covid_trend_list.keys():
+                covid_trend_list[key] = value
+            else:
+                covid_trend_list[key] += value
+
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'donation_before': temp_dict, 'donation_after': covid_trend_list}})
+
+
+
+
+
+
+
 
 def get_top_words(ti):
+
     message = ti.xcom_pull(task_ids='keyword_data', key='message_list')
-    li_message = updated_list_top_words(message,db)
-    print(li_message)
+    new_dict = updated_list_top_words(message,db)
+    print(new_dict)
+
+    date_time_list = ti.xcom_pull(task_ids='make_collection', key='datetime_record')
+    start_datetime = date_time_list['start_datetime']
+
+    batch_list = list(coll_name.find().sort('start_datetime', -1))
+    if len(batch_list) == 1:
+        before_batch = batch_list[0]
+    else:
+        before_batch = batch_list[1]
+
+    print('before start time ', before_batch['start_datetime'])
+    print('abhi wala', start_datetime)
+
+    if before_batch['start_datetime'] == start_datetime:
+        print('1st')
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'top_words_before': 'Null', 'top_words_after': new_dict}})
+
+    else:
+
+        temp_dict = before_batch['top_words_after']
+
+        for key, value in temp_dict.items():
+            if key not in new_dict.keys():
+                new_dict[key] = value
+
+            else:
+                updated_dict = {}
+                old_list = temp_dict[key]
+                for dictionary in old_list:
+                    for key1, val1 in dictionary.items():
+                        updated_dict[key1] = val1
+
+                new_list = new_dict[key]
+                for dictionary in new_list:
+                    for key1, val1 in dictionary.items():
+                        updated_dict[key1] = updated_dict.get(key1, 0) + val1
+
+                new_updated_list = []
+                for key1, val1 in updated_dict.items():
+                    new_updated_list.append({key1: val1})
+
+                new_dict[key] = new_updated_list
+
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'top_words_before': temp_dict, 'top_words_after': new_dict}})
 
 
 def get_donation_data(ti):
     message = ti.xcom_pull(task_ids='keyword_data', key='message_list')
     li_message = updated_donation_list_total_tweets(message,db)
     print(li_message)
+
+    date_time_list = ti.xcom_pull(task_ids='make_collection', key='datetime_record')
+    start_datetime = date_time_list['start_datetime']
+
+    batch_list = list(coll_name.find().sort('start_datetime', -1))
+    if len(batch_list) == 1:
+        before_batch = batch_list[0]
+    else:
+        before_batch = batch_list[1]
+
+    print('before start time ', before_batch['start_datetime'])
+    print('abhi wala', start_datetime)
+
+    if before_batch['start_datetime'] == start_datetime:
+        print('1st')
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'donation_before': 'Null', 'donation_after': li_message}})
+
+    else:
+        temp_dict = before_batch['donation_after']
+        print('purana')
+        for key, value in temp_dict.items():
+            if key not in li_message.keys():
+                li_message[key] = value
+            else:
+                li_message[key] += value
+
+        coll_name.update_one({'start_datetime': start_datetime},
+                             {'$set': {'donation_before': temp_dict, 'donation_after': li_message}})
 
 
 def get_trend_daily(ti):
@@ -192,6 +481,10 @@ top_words = PythonOperator(task_id = 'top_words',python_callable = get_top_words
 donation_coll = PythonOperator(task_id ='donation_coll',python_callable = get_donation_data)
 trend_data_daily = PythonOperator(task_id = 'trend_data_daily',python_callable = get_trend_daily)
 
+# ###################################################
+
+make_collection = PythonOperator(task_id = 'make_collection',python_callable = collection_for_validation)
+
 end = EmptyOperator(task_id='end', dag=dag, trigger_rule=TriggerRule.ONE_SUCCESS)
 
-start >> country_code >> covid_keywords >> donation_data >> prevention_data >> trend_data >> keywords_data >>insert_mongo>> [total_tweets , tweets_daily_basis,top_preventions,trending_data,top_words,donation_coll,trend_data_daily] >> end
+start >> country_code >> [covid_keywords, make_collection] >> donation_data >> prevention_data >> trend_data >> keywords_data >>insert_mongo>> [total_tweets , tweets_daily_basis,top_preventions,trending_data,top_words,donation_coll,trend_data_daily] >> end
