996b4f706eabab27e66a2694fc3474ed91bc407e
saurav-sigmoid1 <sauravv1@sigmoidanalytics.com>
2022-06-09 10:05:20 +0530

    solve conflict in tests

--- test/test_all_extractors.py
+++ test/test_all_extractors.py
@@ -6,16 +6,9 @@ from test.test_donation_amount_and_currency import TestDonationDetails
 from test.test_prevention_keyword import TestPreventionKeywords
 from test.test_trending_keywords import TestTrendingKeywords
 from test.test_encryption_and_decryption import TestEncryptionDecryptionMethod
-
-<<<<<<< HEAD
 from test.test_tweet_keywords_extractor import TestTweetKeywords
-=======
-# from test import TestCountryCode
-# from test import TestCovidKeywords
-# from test import TestDonationDetails
-# from test import TestPreventionKeywords
-# from test import TestTrendingKeywords
->>>>>>> 29e125c59795c3b81a9c7f4bf54b0b33ab6e5db0
+
+
 
 
 class TestAllConsumers(unittest.TestCase):
@@ -27,12 +20,8 @@ class TestAllConsumers(unittest.TestCase):
     TestPreventionKeywords
     TestTrendingKeywords
     TestEncryptionDecryptionMethod
-<<<<<<< HEAD
     TestTweetKeywords
-=======
 
-
->>>>>>> 29e125c59795c3b81a9c7f4bf54b0b33ab6e5db0
 
 if __name__ == '__main__':
     unittest.main()
--- test/test_country_code.py
+++ test/test_country_code.py
@@ -1,10 +1,4 @@
 import unittest
-<<<<<<< HEAD
-=======
-import os
-
-
->>>>>>> 29e125c59795c3b81a9c7f4bf54b0b33ab6e5db0
 from src.common.variable_files import COUNTRY_CODE_KEY
 from src.pub_sub.data_extract.country_code_extractor import get_country_code
 
@@ -12,7 +6,6 @@ from src.pub_sub.data_extract.country_code_extractor import get_country_code
 class TestCountryCode(unittest.TestCase):
 
     def test_country_code_case_1(self):
-<<<<<<< HEAD
         # with correct country name
         message = {'tweet': 'hi', 'country': 'india'}
         result = get_country_code(message)
@@ -26,23 +19,6 @@ class TestCountryCode(unittest.TestCase):
 
 
     def test_country_code_case_3(self):
-=======
-
-        # with correct country name
-        message = {'tweet': 'hi', 'country': 'india'}
-        result = get_country_code(message)
-        self.assertEqual(result[COUNTRY_CODE_KEY],'IN')
-
-    def test_country_code_case_2(self):
-
-        # wrong country name
-        message = {'tweet': 'hi', 'country': 'new york'}
-        result = get_country_code(message)
-        self.assertEqual(result[COUNTRY_CODE_KEY],'No Country')
-
-    def test_country_code_case_3(self):
-
->>>>>>> 29e125c59795c3b81a9c7f4bf54b0b33ab6e5db0
         # case insenstive testing
 
         message = {'tweet': 'hi', 'country': 'INDIa'}
@@ -50,10 +26,7 @@ class TestCountryCode(unittest.TestCase):
         self.assertEqual(result[COUNTRY_CODE_KEY], 'IN')
 
 
-<<<<<<< HEAD
+
 # if __name__ == '__main__':
 #     unittest.main()
-=======
-if __name__ == '__main__':
-    unittest.main()
->>>>>>> 29e125c59795c3b81a9c7f4bf54b0b33ab6e5db0
+
